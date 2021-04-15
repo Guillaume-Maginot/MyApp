d0a393c7183904a89c8b77d65c1e22ea8b413e0a
guillaume-Maginot <g.maginot@gmail.com>
2021-04-15 17:33:39 +0200

    First commit

---
+++ app/src/main/java/com/maginot/myapp/Choixclasse.java
@@ -0,0 +1,14 @@
+package com.maginot.myapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Choixclasse extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_choixclasse);
+    }
+}
